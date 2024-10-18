fa31a5e7637a133112f56692f1f278ac38e93850
lin.hanchao <lin.hanchao@byd.com>
2024-10-18 13:12:17 +0800

    🐞 fix: 修正权重无效问题

--- bot/msg/roles/index.ts
+++ bot/msg/roles/index.ts
@@ -1,58 +1,62 @@
-import FishPi, { ChatMsg } from 'fishpi';
-import * as glob from 'glob';
+import FishPi, { ChatMsg } from "fishpi";
+import * as glob from "glob";
 import * as path from "path";
 import * as fs from "fs";
 const rolePath = path.relative(process.cwd(), __dirname);
-const files = glob.sync(path.join(rolePath, '*.ts').replace(/\\/g, '/'));
+const files = glob.sync(path.join(rolePath, "*.ts").replace(/\\/g, "/"));
 
 export interface MsgRole {
   /**
    * 正则匹配规则
    */
-  match: RegExp[],
+  match: RegExp[];
   /**
    * 规则执行
    * @param msg 红包消息
    * @param fishpi FishPi实例
    */
-  exec: (msg: ChatMsg, fishpi: FishPi) => Promise<boolean> | void,
+  exec: (msg: ChatMsg, fishpi: FishPi) => Promise<boolean> | void;
   /**
    * 是否启用
    */
-  enable?: boolean,
-  
+  enable?: boolean;
+
   /**
    * 权重
    */
-  priority?: number,
+  priority?: number;
 }
 
-const roles :MsgRole[] = [];
-files.forEach(async (file) => {
-  file = './' + path.basename(file, '.ts');
-  if (file === './index') return;
-  const role = (await import(file)).default;
-  roles.push(...role);
-});
-roles.sort((a, b) => (b.priority ?? 0) - (a.priority ?? 0));
+const roles: MsgRole[] = [];
+Promise.all(
+  files.map(async (file) => {
+    file = "./" + path.basename(file, ".ts");
+    if (file === "./index") return;
+    const role = (await import(file)).default;
+    roles.push(...role);
+  })
+).then(() => roles.sort((a, b) => (b.priority ?? 0) - (a.priority ?? 0)));
 
 /**
  * 加载指定目录下的所有消息 Role
  * @param dir 消息 Role 目录
  * @param replace 是否覆盖已有的消息 Role
  */
-export function load(dir: string, replace=true) {
-  if (fs.existsSync(dir) === false) return console.error(`roles dir ${dir} not exist`);
-  const roleFiles = glob.sync(path.join(dir, '*.ts').replace(/\\/g, '/'));
+export function load(dir: string, replace = true) {
+  if (fs.existsSync(dir) === false)
+    return console.error(`roles dir ${dir} not exist`);
+  const roleFiles = glob.sync(path.join(dir, "*.ts").replace(/\\/g, "/"));
   if (replace) roles.splice(0, roles.length);
-  roleFiles.forEach(async (file) => {
-    file = './' + path.basename(file, '.ts');
-    if (file === './index') return;
-    const role = (await import(file)).default;
-    if (role.match === undefined || role.exec === undefined) 
-      return console.error(`role ${file} is not a valid role`);
-    roles.push(...role);
-  });  
+  Promise.all(
+    roleFiles.map(async (file) => {
+      file = "./" + path.basename(file, ".ts");
+      if (file === "./index") return;
+      const role = (await import(file)).default;
+      if (role.match === undefined || role.exec === undefined)
+        return console.error(`role ${file} is not a valid role`);
+      roles.push(...role);
+    })
+  ).then(() => roles.sort((a, b) => (b.priority ?? 0) - (a.priority ?? 0)));
 }
 
 export default roles;
